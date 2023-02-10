ef0b2a7fedc7814196cef6ab1c4536ac0ec3a8b6
Nathália Santos <63311872+NathSantos@users.noreply.github.com>
2023-02-10 10:52:35 -0400

    Update Trabalho12-LPA.cpp

--- Trabalho12-LPA.cpp
+++ Trabalho12-LPA.cpp
@@ -1,3 +1,16 @@
+/*
+PARA EXECUTAR O PROGRAMA VOCÊ PRECISA:
+
+1 - OpenCV instalado:
+$ sudo apt install libopencv-dev
+
+2 - Compilar o código da seguinte forma:
+$ g++ Trabalho12-LPA.cpp -o Trabalho12-LPA `pkg-config --cflags --libs opencv4`
+
+3 - Executar o código da seguinte forma (onde Stars.png é a imagem do céu com estrelas):
+$ ./Trabalho12-LPA Stars.png
+*/
+
 #include <stdio.h>
 
 #include <opencv2/core.hpp>
